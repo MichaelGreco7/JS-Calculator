c4f87b7f4083e35169a103270ded0e643b577ec6
Michael Greco <michael.greco07@gmail.com>
2020-02-04 16:07:55 -0500

    updates

--- app.js
+++ app.js
@@ -9,5 +9,19 @@ const clearBtn = document.querySelector(".btn-clear");
 for (let i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
     let number = btns[i].getAttribute("data-num");
+    screen.value += number;
   });
 }
+
+equalBtn.addEventListener("click", function() {
+  if (screen.value === "") {
+    alert("Input Is Empty");
+  } else {
+    let value = eval(screen.value);
+    screen.value = value;
+  }
+});
+
+clearBtn.addEventListener("click", function() {
+  screen.value = "";
+});
